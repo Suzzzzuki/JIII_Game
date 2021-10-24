586302d70cba4a85db4ee67de5b8803192c826c2
Toshitake SUZUKI <90138047+Suzzzzuki@users.noreply.github.com>
2021-10-24 18:44:37 +0900

    Add CountUP
    計測機能の追加

--- JIsshuPrototype/Assets/TimeController.cs
+++ JIsshuPrototype/Assets/TimeController.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimeController : MonoBehaviour
 {
-    private Text Timetxt;
+    private TextMeshProUGUI Timetxt;
+    private float sec;
+    private float min;
     // Start is called before the first frame update
     void Start()
     {
-        Timetxt = GameObject.Find("Time").GetComponent<Text>();
+        Timetxt = GameObject.Find("Time").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        sec += Time.deltaTime;
+        if (sec > 60){
+            min += 1;
+            sec = 0;
+        }
+
+        Timetxt.text = min.ToString("00") + ":" + sec.ToString("00");
     }
 }
